54b2fc80e909a207559dd3b6a2936fc7a6b62781
Ramadhani <saputraramadhani216@gmail.com>
2023-05-29 10:33:34 +0700

    test

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products/:id", element: <Products /> },
+  { path: "/product/:id", element: <Product /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
